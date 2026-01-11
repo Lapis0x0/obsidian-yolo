8be19f1f91dffc3408edcac3332ace5086126a28
Lapis0x0 <szy2089931398@gmail.com>
2026-01-11 16:25:08 +0800

    发送键回到之前的设计

--- src/components/chat-view/chat-input/SubmitButton.tsx
+++ src/components/chat-view/chat-input/SubmitButton.tsx
@@ -1,13 +1,12 @@
-import { ArrowUp } from 'lucide-react'
+import { CornerDownLeftIcon } from 'lucide-react'
 
 export function SubmitButton({ onClick }: { onClick: () => void }) {
   return (
-    <button
-      className="smtcmp-chat-user-input-submit-button-circle"
-      onClick={onClick}
-      type="button"
-    >
-      <ArrowUp size={16} />
-    </button>
+    <div className="smtcmp-chat-user-input-submit-button" onClick={onClick}>
+      <div className="smtcmp-chat-user-input-submit-button-icons">
+        <CornerDownLeftIcon size={12} />
+      </div>
+      <div>Chat</div>
+    </div>
   )
 }
